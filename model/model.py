0356e76fe1842d433d0c0e74bafb6c5dfdb1a96b
Jordan Lei <thunderflash.lei@gmail.com>
2020-07-21 20:11:01 -0700

    model updates

--- model/model.py
+++ model/model.py
@@ -4,51 +4,35 @@ import pandas as pd
 
 
 # Create dummy confidence data
-def dummy_conf(features = ["prev_conf",  "perf"], samples = 1000):
+def dummy_conf(features = ["conf", "prev_conf",  "perf"], samples = 1000):
     """
     Returns: an np.array of shape (1000 x features)
     """
 
     # Generate specified features
-    prev_conf = np.random.random((samples, 1))
-    perf = np.random.random((samples, 1))
-    exp_perf = prev_conf
+    prev_conf = np.random.normal(0, 1, (samples, 1))
+    perf = np.random.normal(0, 1, (samples, 1))
+    exp_perf = 0.3 * prev_conf
 
     surprise = perf - exp_perf
     
      # Generate confidence from features
-    conf = prev_conf + surprise + np.random.random((samples, 1)) * 0.001
+    conf = prev_conf + surprise + np.random.normal(0, 1, (samples, 1)) * 0.001
     arr = np.hstack([conf, prev_conf, perf])
     df = pd.DataFrame(arr, columns = features)
+
     return df
 
-def dummy_data2(alpha,
-                beta,
-                delta,
-                features = ["conf", "prev_conf",  "acc"], 
-                samples = 1000):
-    """
-    Returns: an np.array of shape (1000 x features)
-    """
-    # previous trial
-    prev_conf = np.random.random((samples, 1))
-    acc = np.random.random((samples, 1))
-    
-    #current trial
-    exp_perf = alpha*prev_conf
-    noise = (np.random.random((samples, 1)) * 0.001)
-    conf = beta*prev_conf + delta*acc + noise
-    arr = np.hstack([conf, prev_conf, acc])
-    df = pd.DataFrame(arr, columns = features)
-    
-    return df
-    
 
-
-
+def plots(df): 
+    for feature in df.columns: 
+        plt.figure()
+        plt.scatter(df[feature], df["conf"])  
+        plt.xlabel(feature)
+        plt.ylabel("conf") 
 
 from sklearn.linear_model import LinearRegression
-def model(x, y, version = "linear"):
+def get_model(x, y, version = "linear"):
     if version == "linear": 
         model = LinearRegression().fit(x, y)
     return model
@@ -57,6 +41,7 @@ def model(x, y, version = "linear"):
 
 def main():
     df = dummy_conf()
+    plt.scatter(df['prev_conf'],df['conf'])
     print(df.head())
     df.describe()
 
