0db5937d4d73e7d42c743a03fddc8857c7cc3853
Jordan Lei <thunderflash.lei@gmail.com>
2020-07-24 21:51:25 -0700

    fixed acceleration

--- preprocess.py
+++ preprocess.py
@@ -74,127 +74,27 @@ def preprocess(alldat, verbose = False):
         contrast_left = np.concatenate([contrast_left, dat["contrast_left"].flatten()])
         contrast_right = np.concatenate([contrast_right, dat["contrast_right"].flatten()])
 
-
         gocue = np.concatenate([gocue, dat["gocue"].flatten() * 1000])
         response_time = np.concatenate([response_time, dat["response_time"].flatten() * 1000])
         response_time_diff = np.concatenate([response_time_diff, np.diff(dat["response_time"].flatten()), [np.nan]])
 
-        
-
         mouse_resp = np.concatenate([mouse_resp, dat["response"].flatten()])
-        wheel_vel = [dat["wheel"][0][t] for t in range(num_trials)]
-        wheel_velocity = wheel_velocity + wheel_vel
-        wheel_acceleration = wheel_acceleration + wheel_vel
+
+        wheel_velocity = wheel_velocity + dat["wheel"][0].tolist()
+        
+        wheel_acc = np.zeros(dat["wheel"][0].shape)* np.nan
+        wheel_acc[:, :-1] = np.diff(dat["wheel"][0])
+
+        wheel_acceleration = wheel_acceleration + wheel_acc.tolist()
         feedback_onset = np.concatenate([feedback_onset, dat["feedback_time"].flatten()])
         feedback_type = np.concatenate([feedback_type, dat["feedback_type"].flatten()])
 
     go_trial = contrast_left * contrast_right != 0
-    print(session.shape)
     session_type = ["test" if x in [1, 4, 8, 12, 19, 22, 25, 30, 35] else "train" for x in session]
     latency = response_time - gocue
     contrast_diff = contrast_left - contrast_right
     pres_difficulty = 1 - np.abs(contrast_diff)
     pres_acc = (feedback_type > 0).astype(int)
-
-  
-
-
-        # for t in range(len(dat.get('gocue'))): #just because the length of this field = number of trials for a given session
-            # session #
-
-
-            # if s == 1 or s== 4 or s == 8 or s == 12 or s == 19 or s == 22 or s== 25 or s == 30 or s == 35:
-            #     session_type.append('test')
-            # else: 
-            #     session_type.append('train') 
-            
-            # mouse name
-            # mouse_name.append(dat.get('mouse_name')) #you know... just the name of the little fellow
-            
-            # trial number
-            # trial_number.append(t+1)      #trial number for that particular session
-
-            # whether it was a go trial or not
-            # go_trial.append((dat.get('contrast_left')[t] != 0) or (dat.get('contrast_right')[t] != 0))
-            
-            # trial type, stimulus location on pres trial, and ideal response
-            # if dat.get('contrast_left')[t] == 0 and dat.get('contrast_right')[t] == 0:
-            #     trial_t= 'F'
-            # elif dat.get('contrast_left')[t] == 0:
-            #     trial_t = 'A'
-            #     stim_l = -1
-            #     ideal_r = -1
-            # elif dat.get('contrast_right')[t] == 0:
-            #     trial_t = 'B'
-            #     stim_l = 1
-            #     ideal_r = 1
-            # elif dat.get('contrast_left')[t] == dat.get('contrast_right')[t] :
-            #     trial_t = 'C'
-            #     stim_l = 2
-            #     ideal_r = np.nan
-            # elif dat.get('contrast_right')[t] > dat.get('contrast_left')[t] :
-            #     trial_t = 'D'
-            #     stim_l = 2
-            #     ideal_r = -1
-            # elif dat.get('contrast_right')[t] < dat.get('contrast_left')[t] :
-            #     trial_t = 'E'
-            #     stim_l= 2
-            #     ideal_r = 1
-            # else:
-            #     trial_t = np.nan
-            #     stim_l = np.nan
-            #     ideal_r = np.nan
-                
-            # trial_type.append(trial_t) 
-            # stim_loc.append(stim_l)
-            # ideal_resp.append(ideal_r)
-            
-            # go cue onset
-            # gocue.append(dat.get('gocue')[t][0]*1000) #the sencond index value is just there so that we can directly access floats from the DF
-            
-            # # mouse response time 
-            # response_time.append(dat.get('response_time')[t][0]*1000)
-            
-            # # latency 
-            # latency.append(dat.get('response_time')[t][0]*1000 - dat.get('gocue')[t][0]*1000)
-
-            # append mouse's response
-            # mouse_resp.append(dat.get('response')[t])
-            
-            # velocity
-            # wheel_velocity.append(dat['wheel'][0][t] * .135) 
-            
-            # wheel acceleration
-            # wheel_acceleration.append(np.diff(wheel_velocity[t]/10))
-        
-            # feedback onset 
-            # feedback_onset.append((dat.get('feedback_time')[t][0]*1000))
-            
-            # feedback type 
-            # feedback_type.append(int(dat.get('feedback_type')[t])) #feedback type: positive (+1) means reward, negative (-1) means white noise burst 
-        
-            # stimulus contrasts and differences between them
-            # contrast_left.append(dat.get('contrast_left')[t])
-            # contrast_right.append(dat.get('contrast_right')[t])
-            
-            # if stim_loc[t] != 0:
-            #     contrast_diff.append((dat.get('contrast_left')[t] - dat.get('contrast_right')[t]).astype(float))
-            # else:
-            #     contrast_diff.append(np.nan)
-
-            # present difficulty
-            # if (dat.get('contrast_left')[t] != 0) or (dat.get('contrast_right')[t] != 0):
-            #     pres_difficulty.append(1-np.abs(dat.get('contrast_left')[t]-dat.get('contrast_right')[t]))
-            # else:
-            #     pres_difficulty.append(np.nan)
-                
-            # # mouse's accuracy on present trial
-            # if dat.get("feedback_type")[t] == 1: 
-            #     mouse_acc_binary = 1
-            #     pres_acc.append(mouse_acc_binary)
-            # else:
-            #     mouse_acc_binary = 0
-            #     pres_acc.append(mouse_acc_binary)
 
     my_dict = {'session': session, 
         'session_type': session_type,
@@ -232,7 +132,6 @@ def preprocess(alldat, verbose = False):
         print(i) 
         print(len(my_dict[i]))
 
-
     df = pd.DataFrame(my_dict)
 
     if verbose: 
@@ -240,7 +139,6 @@ def preprocess(alldat, verbose = False):
             print("column %s dtype: "%(col, df[col].dtype))
 
     return df
-
 ######################################################################################################################
 
 
